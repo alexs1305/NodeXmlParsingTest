67f78524797b393a0e0dce186f90e0a29b871082
Alex <alexs1305@gmail.com>
2016-03-06 10:54:04 +0000

    add section and calls for filtering

--- views/core.js
+++ views/core.js
@@ -4,25 +4,48 @@ var results = angular.module('results',[]);
 
 results.controller("result-controller",function($scope, $http){
     $scope.allResults = {};
+    $scope.allTitles = {};
 
     $http.get('/getresults')
         .success(function(returnedResults) {
                 $scope.allResults = returnedResults;
-                console.log('posted results ' +  returnedResults);
-            })
+                console.log('posted results ' +  $scope.allResults);
+               })
+   $http.get('/gettitles')
+    .success(function(returnedTitles){
+        $scope.allTitles = returnedTitles;
+    });
 
 
     $scope.sendResults = function() {
         $http.post('/addresult', $scope.formData)
             .success(function(returnedResults) {
-                $scope.formData = {};
-                $scope.allResults = returnedResults;
-                console.log('posted results ' +  $scope.allResults);
-            })
+               $scope.formData = {};
+               $scope.allResults = returnedResults;
+               })
             .error(function(data) {
                 console.log('Error: ' + data);
             });
     };
+
+
+    $scope.getAllResults = function(){
+        $http.get('/getresults')
+                .success(function(returnedResults) {
+                        $scope.allResults = returnedResults;
+                        console.log('posted results ' +  $scope.allResults);
+                       });
+    };
+
+    $scope.filterResults = function(filterResults){
+        $http.post('/filterresults/'+filterResults)
+            .success(function(returnedResults) {
+             $scope.formData = {};
+             $scope.allResults = returnedResults;
+             console.log('posted results ' +  $scope.allResults);
+                                       });
+             };
+
 
 
 });
